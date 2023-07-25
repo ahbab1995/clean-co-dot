c5ae72d5e0154cbd4c02750f84a77865fbc46c9c
ahbab1995 <islamahbab1996@yahoo.com>
2023-07-26 00:33:00 +0600

    project on

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="">
+    <h1>Clean Co</h1>
+    <button class="btn btn-accent">Button</button>
+  </div>;
 }
 
 export default App;
