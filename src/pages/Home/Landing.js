855ca5a33a6396dde53f8409dab484fc84ed5acc
ahbab1995 <96840417+ahbab1995@users.noreply.github.com>
2023-08-02 01:07:33 +0600

    new codeed added

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -2,19 +2,71 @@ import React from "react";
 import Bucketgirl from "../../assets/images/bucketgirl.png";
 const Landing = () => {
   return (
-    <div className="hero min-h-screen  bg-base-200">
-      <div className="hero-content mt-10 flex-col lg:flex-row">
-        
-        <div className="mt-8" >
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+    <div className="relative">
+      <div className="hero  h-screen lg:h-[60vh] relative z-10 bg-accent">
+        <div className="hero-content mt-10 flex-col lg:flex-row">
+          <div className="mt-8">
+            <p>Best Quality</p>
+            <h1 className="text-5xl font-bold">
+              Professional Cleaning Service
+            </h1>
+            <p className=" py-6 max-w-xl">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has
+            </p>
+
+            <button className="btn btn-primary">GET STARTED</button>
+          </div>
+          <div className=" lg:h-[60vh] shrink-0">
+            <img src={Bucketgirl} className=" h-full " alt="" />
+          </div>
         </div>
-        <img src={Bucketgirl} className="lg:h-full " alt="" />
+      </div>
+      <div className="shadow-lg w-5/6 mx-auto  bg-base-200 p-10 z-20 mt-[-50] relative rounded-2xl">
+        <h1 className="text-2xl mb-5 text-primary">Get Free Estimate</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+        </div>
+        <button className='btn btn-primary mt-5'>Get a Quote</button>
       </div>
     </div>
   );
